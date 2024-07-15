07bfcbe59fefe3844668478fffb93345208d95c2
John Kelly (XBOX LIVE) <johkelly@microsoft.com>
2024-07-15 09:09:23 -0700

    Update which-key mappings.
    Add fugitive but comment until it works.

--- init.lua
+++ init.lua
@@ -293,19 +293,27 @@ require('lazy').setup({
       require('which-key').setup()
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+      require('which-key').add {
+        { '<leader>c', group = '[c]ode' },
+        { '<leader>c_', hidden = true },
+        { '<leader>d', group = '[d]ocument' },
+        { '<leader>d_', hidden = true },
+        { '<leader>h', group = 'git [h]unk' },
+        { '<leader>h_', hidden = true },
+        { '<leader>r', group = '[r]ename' },
+        { '<leader>r_', hidden = true },
+        { '<leader>s', group = '[s]earch' },
+        { '<leader>s_', hidden = true },
+        { '<leader>t', group = '[t]oggle' },
+        { '<leader>t_', hidden = true },
+        { '<leader>w', group = '[w]orkspace' },
+        { '<leader>w_', hidden = true },
       }
       -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
+      -- require('which-key').register({
+      --   ['<leader>h'] = { 'Git [H]unk' },
+      -- }, { mode = 'v' })
+      require('which-key').add { '<leader>h', desc = 'Git [H]unk', mode = 'v' }
     end,
   },
 
@@ -993,6 +1001,9 @@ require('lazy').setup({
     keys = {
       { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
     },
+    -- {
+    --   'tpope/vim-fugitive',
+    -- },
   },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
